a977ce35f02fe5d50b28c5f5fc1e5f21adaaaf92
leekoby <abc2752@naver.com>
2023-01-28 11:11:21 +0900

    Create OX퀴즈..js

---
+++ leekoby/OX퀴즈..js
@@ -0,0 +1,33 @@
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120907
+function solution(quiz) {
+    let ans = [];
+
+    quiz.forEach((item) => {
+        const oneQuiz = item.split(" ");
+
+        const X = Number(oneQuiz[0]);
+        const Y = Number(oneQuiz[2]);
+
+        let calc = 0;
+
+        if (oneQuiz[1] === "+") {
+            calc = X + Y;
+        }
+
+        if (oneQuiz[1] === "-") {
+            calc = X - Y;
+        }
+
+        const result = Number(oneQuiz[4]);
+
+        if (calc === result) {
+            ans.push("O");
+        }
+
+        if (calc !== result) {
+            ans.push("X");
+        }
+    })
+
+    return ans;
+}
