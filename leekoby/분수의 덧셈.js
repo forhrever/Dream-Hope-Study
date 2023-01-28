95314bea56d4a0312a87291098ca7fbd79fafe2c
leekoby <abc2752@naver.com>
2023-01-28 11:03:58 +0900

    Create 분수의 덧셈.js

---
+++ leekoby/분수의 덧셈.js
@@ -0,0 +1,11 @@
+function solution(denum1, num1, denum2, num2) {
+    var answer = [0, 0];
+    let first = num1 * denum2 + num2 * denum1;
+    let second = num1 * num2;
+    let gcd = (a, b) => (a % b === 0 ? b : gcd(b, a % b));
+    let min = gcd(first, second);
+    answer[0] = first / min;
+    answer[1] = second / min;
+
+    return answer;
+}
